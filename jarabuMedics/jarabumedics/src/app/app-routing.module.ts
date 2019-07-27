7f71064b099d59e21c109d218c3d8acf1112903f
nkongenelly <KENYA@Nellys-MacBook-Pro.local>
2019-07-28 02:39:56 +0300

    Added the login and registation pages using ng command. Added the authenticate service with the register and login functions

--- jarabuMedics/jarabumedics/src/app/app-routing.module.ts
+++ jarabuMedics/jarabumedics/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { HospitalComponent } from './hospital/hospital.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'hospital', component: HospitalComponent },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
 ];
 
 @NgModule({
--- jarabuMedics/jarabumedics/src/app/app.module.ts
+++ jarabuMedics/jarabumedics/src/app/app.module.ts
@@ -8,23 +8,32 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+//Cloud firestore
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { environment } from '../environments/environment';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
-
+//Firebase authentication
+import { AuthenticateService } from './authenticate.service';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { HospitalComponent } from './hospital/hospital.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,
+    HospitalComponent
+  ],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
+    FormsModule,
+    AngularFireAuthModule
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    AuthenticateService,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
