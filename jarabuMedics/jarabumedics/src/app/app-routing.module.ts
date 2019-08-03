1e74f1b6ea0d14989d2f1328c3f3cdf5d695dac7
nkongenelly <KENYA@Nellys-MacBook-Pro.local>
2019-08-03 03:18:28 +0300

    Added 3 tabs at bottom of screen. Home which is the patient registration, all services and contacts

--- jarabuMedics/jarabumedics/src/app/app-routing.module.ts
+++ jarabuMedics/jarabumedics/src/app/app-routing.module.ts
@@ -5,12 +5,15 @@ import { HospitalComponent } from './hospital/hospital.component';
 const routes: Routes = [
   //{ path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: '', loadChildren: './login/login.module#LoginPageModule' },
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
   { path: 'other-registrtaion', loadChildren: './other-registrtaion/other-registrtaion.module#OtherRegistrtaionPageModule' },
   { path: 'menu', loadChildren: './menu/menu.module#MenuPageModule' },
   { path: 'hospital', loadChildren: './hospitals/hospitals.module#HospitalsPageModule' },
   { path: 'menu', loadChildren: './menu/menu.module#MenuPageModule' },
+  { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'contacts', loadChildren: './contacts/contacts.module#ContactsPageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
 
 ];
 
---
+++ jarabuMedics/jarabumedics/src/app/tabs/tabs-routing.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children:
+      [
+        {path: 'home',loadChildren: './home/home.module#HomePageModule'},
+        {path: 'menu',loadChildren: './menu/menu.module#MenuPageModule'},
+        {path: 'contacts',loadChildren: './contacts/contacts.module#ContactsPageModule'},
+        {path: '',loadChildren: './menu/menu.module#MenuPageModule'},
+      ]
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/menu',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  exports:
+  [
+      RouterModule
+  ]
+})
+export class TabsPageRoutingModule {}
